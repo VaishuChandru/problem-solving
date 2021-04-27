1e34b0b1bc1e0b15f2b93a9d77010b2614c0175c
harishsambasivam <imharishsambasivam@gmail.com>
2021-04-27 23:02:39 +0530

    /**

---
+++ leetcode/Remove Duplicates from Sorted Array/Remove Duplicates from Sorted Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let i = 0;
+
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] === nums[j - 1]) {
+      i++;
+    } else {
+      nums[j - i] = nums[j];
+    }
+  }
+
+  return nums.length - i;
+};
